cf7bf593ef50c2c4885326629332d054718d1ef1
Brendan <brplahey@gmail.com>
2024-03-26 16:30:45 -0500

    backend work

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const { login, signup } = require("../controllers/userController");
 
 router.post("/login", login).post("/signup", signup);
+
+module.exports = router;
