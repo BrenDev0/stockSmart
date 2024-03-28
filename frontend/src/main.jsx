a93012dfc9bdd4e2f361316a37153ff6efe4cf87
Brendan <brplahey@gmail.com>
2024-03-28 12:33:11 -0500

    starting Auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalProvider } from "./context/GlobalContext.jsx";
+import { AuthcontexProvider } from "./context/authContext.jsx";
 import GlobalStyle from "./styles/globalStyle.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalProvider>
-      <GlobalStyle />
-      <App />
-    </GlobalProvider>
+    <AuthcontexProvider>
+      <GlobalProvider>
+        <GlobalStyle />
+        <App />
+      </GlobalProvider>
+    </AuthcontexProvider>
   </React.StrictMode>
 );
