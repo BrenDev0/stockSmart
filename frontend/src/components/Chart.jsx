0d94748abc3c661482f9af282f80345d3136a05d
Brendan <brplahey@gmail.com>
2024-04-07 20:12:52 -0500

    same

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import Chart from "chart.js";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  ToolTip,
+} from "recharts";
 
 const Chart = () => {
-  const data = {
-    labels: ["monday", "tuesday", "wedensday", "thursday", "friday"],
-    datasets: [
-      {
-        label: "hello",
-        data: [1, 2, 5, 6, 8],
-        fill: false,
-        borderColor: black,
-      },
-    ],
-  };
-
-  const chart = new Chart({
-    type: line,
-    data: data,
-  });
-
-  return <div></div>;
+  return (
+    <LineChart>
+      <Line />
+    </LineChart>
+  );
 };
 
 export default Chart;
---
+++ frontend/src/pages/News.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const News = () => {
+  return <div></div>;
+};
+
+const NewsStyled = styled.div``;
+
+export default News;
